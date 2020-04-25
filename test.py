ffb5683b66966ffe732b1f2e09411ad3e0fc0881
Moritz Goeckel <moritz.goeckel@gmail.com>
2020-04-25 14:48:29 +0200

    Backspace and some more formatting

--- test.py
+++ test.py
@@ -1,23 +1,65 @@
 import wikipediaapi
 import curses
 import random
+import math
+import sys
 
 wiki_wiki = wikipediaapi.Wikipedia('en')
 next_link = 'Python_(programming_language)'
 
 KEY_ESC = 27
+KEY_BACKSP = 263
+
+C_RED = 1
+C_YELLOW = 2
 
 def getString():
     global next_link
     page_py = wiki_wiki.page(next_link)
     next_link, v = random.choice(list(page_py.links.items()))
-    return page_py.title + " " + page_py.summary
+
+    text = page_py.title + "\n\n" + page_py.summary
+
+    while "\n\n\n" in text:
+        text = text.replace("\n\n\n", "\n\n")
+
+    while "  " in text:
+        text = text.replace("  ", " ")
+
+    #text = text.replace(".", ". ")
+
+    return text
+
+def splitIntoLines(text, textWidth):
+    lines = []
+    linesMeta = []
+    line = ""
+    lineMeta = []
+    for c in text:
+        if c != '\n':
+            line += c
+            lineMeta.append(' ')
+
+        if len(line) > textWidth or (len(line) > textWidth * 0.90 and c == ' ') or c == '\n':
+            lines.append(line)
+            linesMeta.append(lineMeta)
+            line = ""
+            lineMeta = []
+    return lines, linesMeta
 
 # start
 stdscr = curses.initscr()
+
 height, width = stdscr.getmaxyx()
-tw = min(70, width)
-leftMargin = 4
+textWidth = min(50, width)
+leftMargin = math.floor((width - textWidth) / 2)
+topMargin = math.floor(leftMargin / 6)
+
+# print("width=%d height=%d textWidth=%d leftMargin=%d topMargin=%d" % (width, height, textWidth, leftMargin, topMargin))
+
+curses.start_color()
+curses.init_pair(C_RED, curses.COLOR_BLACK, curses.COLOR_RED)
+curses.init_pair(C_YELLOW, curses.COLOR_BLACK, curses.COLOR_YELLOW)
 
 # terminal settings
 curses.noecho()
@@ -27,65 +69,131 @@ stdscr.clear()
 
 # Prepare string
 text = getString()
-# TODO: Remove double spaces, add space after .
-text = text.replace('\n', " ")
-lines = []
-line = ""
-for c in text:
-    line += c
-    if len(line) > tw or (len(line) > tw * 0.90 and c == ' '):
-        lines.append(line)
-        line = ""
+lines, meta = splitIntoLines(text, textWidth)
 
 # print string
-for i in range(0, min(height, len(lines))):
-    stdscr.addstr(i, leftMargin, lines[i])
+for i in range(0, min(height - topMargin, len(lines))):
+    stdscr.addstr(i + topMargin, leftMargin, lines[i])
 
-def setCursor(y, x):
-    global stdscr, curses, leftMargin, lines
+def advanceCursor(y, x):
+    global stdscr, curses, leftMargin, topMargin, lines, meta
+
+    # skip empty lines
+    while len(lines[y]) == 0:
+        y += 1
+
     c = lines[y][x]
-    stdscr.addstr(y, leftMargin + x, lines[y][x], curses.A_REVERSE)
-    x -= 1
-    if x < 0 and y > 0:
-        y -= 1
-        x = len(lines[y]) - 1
+    stdscr.addstr(y + topMargin, leftMargin + x, lines[y][x], curses.A_REVERSE)
 
-    if y >= 0:
-        stdscr.addstr(y, leftMargin + x, lines[y][x])
+    # Redraw one char before
+    x -= 1
+    if x < 0:
+        y -= 1
+        while len(lines[y]) == 0: # Skip empty lines
+            y -= 1
+
+        x = len(lines[y]) - 1
+        if y < 0:
+            return c # Do nothing, start of file
+
+    if meta[y][x] == 'o' or meta[y][x] == ' ': # No meta info
+        stdscr.addstr(topMargin + y, leftMargin + x, lines[y][x])
+    elif meta[y][x] == 'e': # Mistake
+        stdscr.addstr(topMargin + y, leftMargin + x, lines[y][x], curses.color_pair(C_RED))
+    elif meta[y][x] == 'c': # Corrected
+        stdscr.addstr(topMargin + y, leftMargin + x, lines[y][x], curses.color_pair(C_YELLOW))
+
     return c
 
-setCursor(0, 0)
+def setbackCursor(y, x):
+    global stdscr, curses, leftMargin, topMargin, lines, meta
+
+    c = lines[y][x]
+    stdscr.addstr(topMargin + y, leftMargin + x, lines[y][x], curses.A_REVERSE)
+
+    # Redraw one char after
+    x += 1
+    if x > len(lines[y]) - 1:
+        x = 0
+        y += 1
+        while len(lines[y]) == 0: # Skip empty lines
+            y += 1
+        if y >= len(lines):
+            return c # Do nothing, end of file
+
+    if meta[y][x] == 'c' or meta[y][x] == 'e': # Corrected
+        stdscr.addstr(topMargin + y, leftMargin + x, lines[y][x], curses.color_pair(C_YELLOW))
+    else:
+        stdscr.addstr(topMargin + y, leftMargin + x, lines[y][x])
+
+    return c
+
+advanceCursor(0, 0)
 
 # input loop
 x = 0
 y = 0
 currentChar = lines[0][0]
+c = chr(80)
 
 mistakes = 0
+correct = 0
 charsTyped = 0
+
 while True:
     # TODO: accuracy and wpm
-    stdscr.addstr(height - 1, 0, "Chars: " + str(charsTyped) + " Mistakes: " + str(mistakes))
+    stdscr.addstr(height - 1, leftMargin, "Typed: " + str(charsTyped) + " Mistakes: " + str(mistakes) + " Last: " + str(c) + " Current: " + currentChar)
 
     c = stdscr.getch()
     if c == KEY_ESC:
         break
 
-    if chr(c) != currentChar:
-        mistakes += 1
-        # TODO: Add error mode: Still offset cursor, but
-        # draw red and expect backspaces
+    elif c == KEY_BACKSP:
+        x = x - 1
+
+        if x < 0 and y > 0:
+            y -= 1
+            while len(lines[y]) == 0: # Skip empty lines
+                y -= 1
+            x = len(lines[y]) - 1
+
+        if x < 0 and y == 0:
+            x = 0
+
+        if meta[y][x] == 'o':
+            meta[y][x] = ' ' # Undo the Okay
+
+        currentChar = setbackCursor(y, x)
         continue
 
-    x = x + 1
+    elif chr(c) == currentChar:
+        correct += 1
+        if meta[y][x] == 'e' or meta[y][x] == 'c':
+            meta[y][x] = 'c'  # Corrected
+        else:
+            meta[y][x] = 'o'  # Okay
+
+    elif chr(c) != currentChar:
+        mistakes += 1
+        meta[y][x] = 'e' # Mistake
+
+    charsTyped += 1
+
+    # TODO: Refactor, nextCord, prevCord methods
+    x += 1
     if x >= len(lines[y]):
         x = 0
         y += 1
+        while len(lines[y]) == 0: # Skip empty lines
+            y += 1
 
-    currentChar = setCursor(y, x)
-    charsTyped += 1
+    currentChar = advanceCursor(y, x)
+
+    #stdscr.refresh()
     # TODO: End of text > next text
     # TODO: Shortcut to go to next article
+    # TODO: Shortcut to search for article
+    # TODO: Choose links?
 
 # end
 curses.nocbreak()
@@ -93,7 +201,7 @@ stdscr.keypad(0)
 curses.echo()
 curses.endwin()
 
-print(c)
+# print(c)
 
 #print("Page - Title: %s" % page_py.title)
 #print("Page - Summary: %s" % page_py.summary)
